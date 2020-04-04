dd01266adfd86be8623318ea2307aba95c6ba6af
Bekkalizer <markus.bekken@gmail.com>
2020-04-04 23:02:07 +0000

    e9f9662 fix: Added translation for hardcoded texts (#513)

--- app-20103e1ae32f66c57ab6.js
+++ app-20103e1ae32f66c57ab6.js
@@ -22469,7 +22469,7 @@
 	
 	var trackerCapture = angular.module('trackerCapture');
 	
-	trackerCapture.controller('HomeController', ["$rootScope", "$scope", "$modal", "$location", "$filter", "$timeout", "$q", "Paginator", "MetaDataFactory", "DateUtils", "OrgUnitFactory", "ProgramFactory", "AttributesFactory", "EntityQueryFactory", "CurrentSelection", "TEIGridService", "TEIService", "GridColumnService", "ProgramWorkingListService", "TCStorageService", "orderByFilter", "TEService", "AccessUtils", "TeiAccessApiService", function ($rootScope, $scope, $modal, $location, $filter, $timeout, $q, Paginator, MetaDataFactory, DateUtils, OrgUnitFactory, ProgramFactory, AttributesFactory, EntityQueryFactory, CurrentSelection, TEIGridService, TEIService, GridColumnService, ProgramWorkingListService, TCStorageService, orderByFilter, TEService, AccessUtils, TeiAccessApiService) {
+	trackerCapture.controller('HomeController', ["$rootScope", "$scope", "$modal", "$location", "$filter", "$timeout", "$translate", "$q", "Paginator", "MetaDataFactory", "DateUtils", "OrgUnitFactory", "ProgramFactory", "AttributesFactory", "EntityQueryFactory", "CurrentSelection", "TEIGridService", "TEIService", "GridColumnService", "ProgramWorkingListService", "TCStorageService", "orderByFilter", "TEService", "AccessUtils", "TeiAccessApiService", function ($rootScope, $scope, $modal, $location, $filter, $timeout, $translate, $q, Paginator, MetaDataFactory, DateUtils, OrgUnitFactory, ProgramFactory, AttributesFactory, EntityQueryFactory, CurrentSelection, TEIGridService, TEIService, GridColumnService, ProgramWorkingListService, TCStorageService, orderByFilter, TEService, AccessUtils, TeiAccessApiService) {
 	    TeiAccessApiService.setAuditCancelledSettings(null);
 	    $scope.trackedEntityTypesById = {};
 	    var previousProgram = null;
@@ -22478,7 +22478,7 @@
 	
 	    var viewsByType = {
 	        registration: {
-	            name: "Register",
+	            name: $translate.instant('register'),
 	            template: "components/registration/registration.html",
 	            class: "col-lg-10 col-md-12",
 	            shouldReset: false,
@@ -22488,14 +22488,14 @@
 	            }
 	        },
 	        lists: {
-	            name: "Lists",
+	            name: $translate.instant('lists'),
 	            template: "components/home/lists/lists.html",
 	            class: "col-xs-12",
 	            shouldReset: true,
 	            disabled: false
 	        },
 	        search: {
-	            name: "Search",
+	            name: $translate.instant('search'),
 	            template: "components/home/search/search.html",
 	            class: "",
 	            shouldReset: true,
@@ -39897,4 +39897,4 @@
 
 /***/ }
 /******/ ]);
-//# sourceMappingURL=app-64885aa46ed3889bb1c0.js.map
+//# sourceMappingURL=app-20103e1ae32f66c57ab6.js.map
