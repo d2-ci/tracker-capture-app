fc21601e6675ee7b72b561800c8b29e17f40ba38
GitHub <noreply@github.com>
2021-09-13 12:21:29 +0000

    be46206 fix: check that eventRows is defined (#1248)

--- app-287e18e7c97bbdf3b862.js
+++ app-287e18e7c97bbdf3b862.js
@@ -19854,29 +19854,30 @@
 	
 	            EventReportService.getEventReport($scope.selectedOrgUnit.id, $scope.selectedOuMode, $scope.model.selectedProgram.id, null, null, 'ACTIVE', 'OVERDUE', $scope.pager).then(function (data) {
 	                if (data) {
-	                    $scope.pager.toolBarDisplay = 5;
-	                    $scope.pager.recordsCount = data.eventRows.length;
+	                    if (data.eventRows) {
+	                        $scope.pager.toolBarDisplay = 5;
+	                        $scope.pager.recordsCount = data.eventRows.length;
 	
-	                    angular.forEach(data.eventRows, function (row) {
-	                        var overdueEvent = {};
-	                        angular.forEach(row.attributes, function (att) {
-	                            if (att.attribute && $scope.attributesById[att.attribute]) {
-	                                att.value = CommonUtils.formatDataValue(null, att.value, $scope.attributesById[att.attribute], $scope.optionSets, 'USER');
-	                            }
-	                            overdueEvent[att.attribute] = att.value;
+	                        angular.forEach(data.eventRows, function (row) {
+	                            var overdueEvent = {};
+	                            angular.forEach(row.attributes, function (att) {
+	                                if (att.attribute && $scope.attributesById[att.attribute]) {
+	                                    att.value = CommonUtils.formatDataValue(null, att.value, $scope.attributesById[att.attribute], $scope.optionSets, 'USER');
+	                                }
+	                                overdueEvent[att.attribute] = att.value;
+	                            });
+	
+	                            overdueEvent.dueDate = DateUtils.formatFromApiToUser(row.dueDate);
+	                            overdueEvent.event = row.event;
+	                            overdueEvent.eventName = $scope.programStages[row.programStage].displayName;
+	                            overdueEvent.orgUnitName = row.orgUnitName;
+	                            overdueEvent.followup = row.followup;
+	                            overdueEvent.program = row.program;
+	                            overdueEvent.programStage = row.programStage;
+	                            overdueEvent.trackedEntityInstance = row.trackedEntityInstance;
+	                            $scope.overdueEvents.push(overdueEvent);
 	                        });
-	
-	                        overdueEvent.dueDate = DateUtils.formatFromApiToUser(row.dueDate);
-	                        overdueEvent.event = row.event;
-	                        overdueEvent.eventName = $scope.programStages[row.programStage].displayName;
-	                        overdueEvent.orgUnitName = row.orgUnitName;
-	                        overdueEvent.followup = row.followup;
-	                        overdueEvent.program = row.program;
-	                        overdueEvent.programStage = row.programStage;
-	                        overdueEvent.trackedEntityInstance = row.trackedEntityInstance;
-	                        $scope.overdueEvents.push(overdueEvent);
-	                    });
-	
+	                    }
 	                    //sort overdue events by their due dates - this is default
 	                    if (!$scope.sortColumn.id) {
 	                        $scope.sortGrid({ id: 'dueDate', displayName: $translate.instant('due_date'), valueType: 'DATE', displayInListNoProgram: false, showFilter: false, show: true });
@@ -20110,29 +20111,31 @@
 	        $scope.upcomingEvents = [];
 	        EventReportService.getEventReport($scope.selectedOrgUnit.id, $scope.selectedOuMode, $scope.model.selectedProgram.id, DateUtils.formatFromUserToApi($scope.report.startDate), DateUtils.formatFromUserToApi($scope.report.endDate), 'ACTIVE', 'SCHEDULE', $scope.pager).then(function (data) {
 	            if (data) {
-	                $scope.pager.toolBarDisplay = 5;
-	                $scope.pager.recordsCount = data.eventRows.length;
+	                if (data.eventRows) {
+	                    $scope.pager.toolBarDisplay = 5;
+	                    $scope.pager.recordsCount = data.eventRows.length;
 	
-	                angular.forEach(data.eventRows, function (row) {
-	                    var upcomingEvent = {};
-	                    angular.forEach(row.attributes, function (att) {
-	                        if (att.attribute && $scope.attributesById[att.attribute]) {
-	                            att.value = CommonUtils.formatDataValue(null, att.value, $scope.attributesById[att.attribute], $scope.optionSets, 'USER');
-	                        }
-	                        upcomingEvent[att.attribute] = att.value;
+	                    angular.forEach(data.eventRows, function (row) {
+	                        var upcomingEvent = {};
+	                        angular.forEach(row.attributes, function (att) {
+	                            if (att.attribute && $scope.attributesById[att.attribute]) {
+	                                att.value = CommonUtils.formatDataValue(null, att.value, $scope.attributesById[att.attribute], $scope.optionSets, 'USER');
+	                            }
+	                            upcomingEvent[att.attribute] = att.value;
+	                        });
+	
+	                        upcomingEvent.dueDate = DateUtils.formatFromApiToUser(row.dueDate);
+	                        upcomingEvent.event = row.event;
+	                        upcomingEvent.eventName = $scope.programStages[row.programStage].displayName;
+	                        upcomingEvent.orgUnitName = row.orgUnitName;
+	                        upcomingEvent.followup = row.followup;
+	                        upcomingEvent.program = row.program;
+	                        upcomingEvent.programStage = row.programStage;
+	                        upcomingEvent.trackedEntityInstance = row.trackedEntityInstance;
+	                        upcomingEvent.created = DateUtils.formatFromApiToUser(row.registrationDate);
+	                        $scope.upcomingEvents.push(upcomingEvent);
 	                    });
-	
-	                    upcomingEvent.dueDate = DateUtils.formatFromApiToUser(row.dueDate);
-	                    upcomingEvent.event = row.event;
-	                    upcomingEvent.eventName = $scope.programStages[row.programStage].displayName;
-	                    upcomingEvent.orgUnitName = row.orgUnitName;
-	                    upcomingEvent.followup = row.followup;
-	                    upcomingEvent.program = row.program;
-	                    upcomingEvent.programStage = row.programStage;
-	                    upcomingEvent.trackedEntityInstance = row.trackedEntityInstance;
-	                    upcomingEvent.created = DateUtils.formatFromApiToUser(row.registrationDate);;
-	                    $scope.upcomingEvents.push(upcomingEvent);
-	                });
+	                }
 	
 	                //sort upcoming events by their due dates - this is default
 	                if (!$scope.sortColumn.id) {
@@ -39393,4 +39396,4 @@
 
 /***/ })
 /******/ ]);
-//# sourceMappingURL=app-fb439645d14e8a826309.js.map
+//# sourceMappingURL=app-287e18e7c97bbdf3b862.js.map
