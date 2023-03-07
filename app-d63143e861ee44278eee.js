03b07a7b7a43bbcfe15164ff271535a5678f6235
GitHub <noreply@github.com>
2023-03-07 22:10:51 +0000

    e76583e fix: [DHIS2-14596] use English as fallback in date picker (#1674)

--- app-d63143e861ee44278eee.js
+++ app-d63143e861ee44278eee.js
@@ -587,6 +587,8 @@
 	
 	/* service for getting calendar setting */
 	.service('CalendarService', ["storage", "SessionStorageService", "$rootScope", function (storage, SessionStorageService, $rootScope) {
+	    // The following array should be manually kept in sync with the one in `index.ejs`
+	    var supportedCalendarLocales = ['ar', 'ar-EG', 'zn-CH', 'cs', 'da', 'nl', 'fr', 'km', 'lo', 'nb', 'pt-BR', 'ro', 'ru', 'es', 'sv', 'uk', 'ur', 'vi'];
 	
 	    return {
 	        getSetting: function getSetting() {
@@ -596,6 +598,11 @@
 	            var userSettings = SessionStorageService.get('USER_SETTING');
 	
 	            dhis2CalendarFormat.locale = userSettings.keyUiLocale.replace('_', '-');
+	            if (!supportedCalendarLocales.find(function (locale) {
+	                return locale === dhis2CalendarFormat.locale;
+	            })) {
+	                dhis2CalendarFormat.locale = 'en';
+	            }
 	
 	            if (angular.isObject(storedFormat) && storedFormat.keyDateFormat && storedFormat.keyCalendar) {
 	                if (storedFormat.keyCalendar === 'iso8601') {
@@ -40945,4 +40952,4 @@
 
 /***/ })
 /******/ ]);
-//# sourceMappingURL=app-bc665a8bb024e8443ed1.js.map
+//# sourceMappingURL=app-d63143e861ee44278eee.js.map
