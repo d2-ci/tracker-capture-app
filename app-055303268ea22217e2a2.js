3fd49c3f42e63d7c0ae1aedcf6aeb6a2dd2d4ddf
GitHub <noreply@github.com>
2023-08-23 14:16:38 +0000

    3a5768f fix: [TECH-1627] use only valid combinations of orgUnit and ouMode

--- app-055303268ea22217e2a2.js
+++ app-055303268ea22217e2a2.js
@@ -9193,6 +9193,7 @@
 	        return enrollment;
 	    };
 	    var errorHeader = $translate.instant("error");
+	    var exclusiveOuModes = ['ACCESSIBLE', 'CAPTURE', 'ALL'];
 	    return {
 	        get: function get(enrollmentUid, teiUid, programUid) {
 	            var url = DHIS2URL + '/enrollments/' + enrollmentUid;
@@ -9210,8 +9211,15 @@
 	            });
 	        },
 	        getByStartAndEndDate: function getByStartAndEndDate(program, orgUnit, ouMode, startDate, endDate, pageSize) {
+	            var url = DHIS2URL + '/enrollments.json?program=' + program;
 	            var paging = pageSize ? '&pageSize=' + pageSize : '&paging=false';
-	            var promise = $http.get(DHIS2URL + '/enrollments.json?program=' + program + '&ou=' + orgUnit + '&ouMode=' + ouMode + '&programStartDate=' + startDate + '&programEndDate=' + endDate + '&fields=:all' + paging).then(function (response) {
+	            if (orgUnit && !exclusiveOuModes.includes(ouMode)) {
+	                url = url + '&ou=' + orgUnit;
+	            }
+	            if (ouMode) {
+	                url = url + '&ouMode=' + ouMode;
+	            }
+	            var promise = $http.get(url + '&programStartDate=' + startDate + '&programEndDate=' + endDate + '&fields=:all' + paging).then(function (response) {
 	                return convertFromApiToUser(response.data);
 	            }, function (response) {
 	                var errorBody = $translate.instant('failed_to_fetch_enrollment');
@@ -9427,43 +9435,47 @@
 	.factory('TEIService', ["$http", "$translate", "DHIS2URL", "$q", "AttributesFactory", "CommonUtils", "CurrentSelection", "DateUtils", "NotificationService", "TeiAccessApiService", function ($http, $translate, DHIS2URL, $q, AttributesFactory, CommonUtils, CurrentSelection, DateUtils, NotificationService, TeiAccessApiService) {
 	    var cachedTeiWithProgramData = null;
 	    var errorHeader = $translate.instant("error");
+	    var exclusiveOuModes = ['ACCESSIBLE', 'CAPTURE', 'ALL'];
 	    var getSearchUrl = function getSearchUrl(type, ouId, ouMode, queryUrl, programOrTETUrl, attributeUrl, pager, paging, format) {
 	        var baseUrl = DHIS2URL + '/trackedEntityInstances/' + type;
 	        var url = baseUrl;
+	        var params = [];
 	        var deferred = $q.defer();
 	
 	        if (format === "csv") {
-	            url = url + '.csv?ou=' + ouId + '&ouMode=' + ouMode;
+	            url = url + '.csv?';
 	        } else if (format === "xml") {
-	            url = url + '.json?ou=' + ouId + '&ouMode=' + ouMode;
+	            url = url + '.json?';
 	        } else {
-	            url = url + '.json?ou=' + ouId + '&ouMode=' + ouMode;
+	            url = url + '.json?';
 	        }
 	
+	        if (ouId && !exclusiveOuModes.includes(ouMode)) {
+	            params.push('ou=' + ouId);
+	        }
+	        if (ouMode) {
+	            params.push('ouMode=' + ouMode);
+	        }
 	        if (queryUrl) {
-	            url = url + '&' + queryUrl;
+	            params.push(queryUrl);
 	        }
 	        if (programOrTETUrl) {
-	            url = url + '&' + programOrTETUrl;
+	            params.push(programOrTETUrl);
 	        }
 	        if (attributeUrl) {
-	            url = url + '&' + attributeUrl;
+	            params.push(attributeUrl);
 	        }
 	        if (paging) {
 	            var pgSize = pager && pager.pageSize || 50;
 	            var pg = pager && pager.page || 1;
 	            pgSize = pgSize > 1 ? pgSize : 1;
 	            pg = pg > 1 ? pg : 1;
-	            url = url + '&pageSize=' + pgSize + '&page=' + pg;
-	            if (pager && pager.skipTotalPages) {
-	                url += '&totalPages=false';
-	            } else {
-	                url += '&totalPages=true';
-	            }
+	            params.push('pageSize=' + pgSize + '&page=' + pg);
+	            params.push('totalPages=' + !(pager && pager.skipTotalPages));
 	        } else {
-	            url = url + '&paging=false';
+	            params.push('paging=false');
 	        }
-	        return url;
+	        return url + params.join('&');
 	    };
 	    var setTeiAttributeValues = function setTeiAttributeValues(teiAttributes, optionSets, attributesById) {
 	        teiAttributes.forEach(function (att) {
@@ -9931,6 +9943,7 @@
 	
 	    var skipPaging = "&skipPaging=true";
 	    var errorHeader = $translate.instant("error");
+	    var exclusiveOuModes = ['ACCESSIBLE', 'CAPTURE', 'ALL'];
 	
 	    var getContextEvent = function getContextEvent(dhis2Event) {
 	        if (Array.isArray(dhis2Event)) {
@@ -9942,7 +9955,7 @@
 	    return {
 	
 	        getEventsByStatus: function getEventsByStatus(entity, orgUnit, program, programStatus) {
-	            var promise = TeiAccessApiService.get(entity, program, DHIS2URL + '/events.json?ouMode=ACCESSIBLE&' + 'trackedEntityInstance=' + entity + '&orgUnit=' + orgUnit + '&program=' + program + '&programStatus=' + programStatus + skipPaging).then(function (response) {
+	            var promise = TeiAccessApiService.get(entity, program, DHIS2URL + '/events.json?ouMode=ACCESSIBLE&' + 'trackedEntityInstance=' + entity + '&program=' + program + '&programStatus=' + programStatus + skipPaging).then(function (response) {
 	                return response.data.events;
 	            }, function (response) {
 	
@@ -9985,14 +9998,18 @@
 	            return promise;
 	        },
 	        getByOrgUnitAndProgram: function getByOrgUnitAndProgram(orgUnit, ouMode, program, startDate, endDate, pageSize) {
-	            var url;
-	            var paging = pageSize ? '&pageSize=' + pageSize : skipPaging;
-	            if (startDate && endDate) {
-	                url = DHIS2URL + '/events.json?' + 'orgUnit=' + orgUnit + '&ouMode=' + ouMode + '&program=' + program + '&startDate=' + startDate + '&endDate=' + endDate + paging;
-	            } else {
-	                url = DHIS2URL + '/events.json?' + 'orgUnit=' + orgUnit + '&ouMode=' + ouMode + '&program=' + program + paging;
+	            var url = DHIS2URL + '/events.json?program=' + program;
+	            if (orgUnit && !exclusiveOuModes.includes(ouMode)) {
+	                url = url + '&orgUnit=' + orgUnit;
 	            }
-	            var promise = $http.get(url).then(function (response) {
+	            if (ouMode) {
+	                url = url + '&ouMode=' + ouMode;
+	            }
+	            if (startDate && endDate) {
+	                url = url + '&startDate=' + startDate + '&endDate=' + endDate;
+	            }
+	            var paging = pageSize ? '&pageSize=' + pageSize : skipPaging;
+	            var promise = $http.get(url + paging).then(function (response) {
 	                return response.data.events;
 	            }, function (response) {
 	                if (response && response.data && response.data.status === 'ERROR') {
@@ -10098,11 +10115,20 @@
 	/* factory for handling event reports */
 	.factory('EventReportService', ["$http", "DHIS2URL", "$translate", "NotificationService", function ($http, DHIS2URL, $translate, NotificationService) {
 	    var errorHeader = $translate.instant("error");
+	    var exclusiveOuModes = ['ACCESSIBLE', 'CAPTURE', 'ALL'];
 	    return {
 	
 	        getEventReport: function getEventReport(orgUnit, ouMode, program, startDate, endDate, programStatus, eventStatus, pager) {
 	
-	            var url = DHIS2URL + '/events/eventRows.json?' + 'orgUnit=' + orgUnit + '&ouMode=' + ouMode + '&program=' + program;
+	            var url = DHIS2URL + '/events/eventRows.json?program=' + program;
+	
+	            if (orgUnit && !exclusiveOuModes.includes(ouMode)) {
+	                url = url + '&orgUnit=' + orgUnit;
+	            }
+	
+	            if (ouMode) {
+	                url = url + '&ouMode=' + ouMode;
+	            }
 	
 	            if (programStatus) {
 	                url = url + '&programStatus=' + programStatus;
@@ -41054,4 +41080,4 @@
 
 /***/ })
 /******/ ]);
-//# sourceMappingURL=app-7aacdb59dc80f8727210.js.map
+//# sourceMappingURL=app-055303268ea22217e2a2.js.map
